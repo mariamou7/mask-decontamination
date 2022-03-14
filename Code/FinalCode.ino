da47b309fbf3982dd75e14517321c097271c2424
Maria-Anastasia Moustaka <mariamou7@windowslive.com>
2022-03-14 12:41:50 +0200

    Create FinalCode.ino

---
+++ Code/FinalCode.ino
@@ -0,0 +1,21 @@
+/*
+  Mask decontaminator box final code
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Author: dECOntaminator team
+*/
+
+//Initializations
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+
+}
